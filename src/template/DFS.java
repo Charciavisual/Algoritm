a16b71a39556dd88c032d0f254eb2843d01f8cf4
최창희 <charciavisual@icloud.com>
2019-10-16 01:34:30 +0900

    DFS template (recursive, iterative)

---
+++ src/template/DFS.java
@@ -0,0 +1,78 @@
+package template;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DFS {
+    private ArrayList<Integer>[] adj;
+    private boolean[] visited;
+
+    public DFS(int n) {
+        adj = new ArrayList[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+    }
+
+    public boolean add(int vFrom, int vTo) {
+        adj[vFrom].add(vTo);
+        return true;
+    }
+
+    private void recurDfs(int curPos) {
+        System.out.println(curPos);
+        visited[curPos] = true;
+
+        for (int i = 0; i < adj[curPos].size(); i++) {
+            int next = adj[curPos].get(i);
+            if (!visited[next])
+                recurDfs(next);
+        }
+    }
+
+    private void iterDfs(int curPos) {
+        Stack<Integer> stack = new Stack<>();
+        boolean flag = false;
+
+        System.out.println(curPos);
+        visited[curPos] = true;
+        stack.push(curPos);
+
+        while (!stack.isEmpty()){
+            curPos = stack.peek();
+            flag = false;
+            for (int i = 0; i < adj[curPos].size(); i++) {
+                int next = adj[curPos].get(i);
+                if (!visited[next]) {
+                    System.out.println(next);
+                    visited[next] = true;
+                    stack.push(next);
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(!flag)
+                stack.pop();
+        }
+    }
+
+    public void recurDfs() {
+        Arrays.fill(visited, false);
+        for (int i = 0; i < adj.length; i++) {
+            if (!visited[i])
+                recurDfs(i);
+        }
+    }
+
+    public void iterDfs() {
+        Arrays.fill(visited, false);
+        for (int i = 0; i < adj.length; i++) {
+            if (!visited[i])
+                iterDfs(i);
+        }
+    }
+}
