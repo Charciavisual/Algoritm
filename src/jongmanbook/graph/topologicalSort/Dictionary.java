bf557727b943c3a3fafd4c44d0e6a780b465c0b8
최창희 <charciavisual@icloud.com>
2019-10-16 15:52:06 +0900

    위상정렬 - 고대어사전 수정

--- src/jongmanbook/graph/topologicalSort/Dictionary.java
+++ src/jongmanbook/graph/topologicalSort/Dictionary.java
@@ -7,73 +7,87 @@ import java.util.Stack;
 
 public class Dictionary {
 
-    private int[][] adj;
-    private ArrayList<Integer> order;
-    private boolean[] visited;
-    private int numOfData;
+    public void solve(int n, String[] words) {
 
-    public void init() {
-        adj = new int[26][26];
-        order = new ArrayList<>();
-        visited = new boolean[26];
-        numOfData = 26;
-    }
+        TopologicalSort ts = new TopologicalSort(26);
 
-    public boolean add(String s1, String s2) {
+        for (int i = 1; i < n; i++) {
+            int len = Math.min(words[i - 1].length(), words[i].length());
 
-        int len = Math.min(s1.length(), s2.length());
-
-        for (int i = 0; i < len; i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                int a = s1.charAt(i) - 'a';
-                int b = s2.charAt(i) - 'a';
-                adj[a][b] = 1;
-                break;
-            }
-        }
-
-        return true;
-    }
-
-    private void iterativeDfs(int curPos) {
-        Stack<Integer> stack = new Stack<>();
-        boolean flag = false;
-
-        visited[curPos] = true;
-        stack.push(curPos);
-
-        while (!stack.isEmpty()) {
-            curPos = stack.peek();
-            flag = false;
-            for (int next = 0; next < numOfData; next++) {
-                if (adj[curPos][next] == 1 && !visited[next]) {
-                    visited[next] = true;
-                    stack.push(next);
-                    flag = true;
+            for (int k = 0; k < len; k++) {
+                if (words[i - 1].charAt(k) != words[i].charAt(k)) {
+                    int a = words[i - 1].charAt(k) - 'a';
+                    int b = words[i].charAt(k) - 'a';
+                    ts.add(a, b);
                     break;
                 }
             }
-
-            if (!flag) {
-                order.add(stack.pop());
-            }
         }
+
+        ArrayList<Integer> result = ts.topologicalSort();
+        if (result.size() == 0) System.out.println("INVALID HYPOTHESIS");
+        else result.forEach(node -> System.out.print((char) (node + 'a')));
     }
 
-    public ArrayList<Integer> topologicalSort() {
-        Arrays.fill(visited, false);
-        order.clear();
-        for (int i = 0; i < numOfData; i++) {
-            if (!visited[i])
-                iterativeDfs(i);
+    class TopologicalSort {
+
+        private int[][] adj;
+        private ArrayList<Integer> order;
+        private boolean[] visited;
+        private int numOfData;
+
+        public TopologicalSort(int n) {
+            adj = new int[n][n];
+            order = new ArrayList<>();
+            visited = new boolean[n];
+            numOfData = n;
         }
 
-        Collections.reverse(order);
+        public boolean add(int v1, int v2) {
+            adj[v1][v2] = 1;
+            return true;
+        }
 
-        for (int i = 0; i < order.size(); i++)
-            for (int j = i + 1; j < order.size(); j++) {
-                if (adj[order.get(j)][order.get(i)] == 1) return new ArrayList<>();
+        private void iterativeDfs(int curPos) {
+            Stack<Integer> stack = new Stack<>();
+            boolean flag = false;
+
+            visited[curPos] = true;
+            stack.push(curPos);
+
+            while (!stack.isEmpty()) {
+                curPos = stack.peek();
+                flag = false;
+                for (int next = 0; next < numOfData; next++) {
+                    if (adj[curPos][next] == 1 && !visited[next]) {
+                        visited[next] = true;
+                        stack.push(next);
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag) {
+                    order.add(stack.pop());
+                }
             }
-        return order;
+        }
+
+        public ArrayList<Integer> topologicalSort() {
+            Arrays.fill(visited, false);
+            order.clear();
+            for (int i = 0; i < numOfData; i++) {
+                if (!visited[i])
+                    iterativeDfs(i);
+            }
+
+            Collections.reverse(order);
+
+            for (int i = 0; i < order.size(); i++)
+                for (int j = i + 1; j < order.size(); j++) {
+                    if (adj[order.get(j)][order.get(i)] == 1) return new ArrayList<>();
+                }
+            return order;
+        }
     }
 }
