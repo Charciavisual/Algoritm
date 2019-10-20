7bf9df2285320c162a5b7d2f8ad2cf54e504e0db
최창희 <charciavisual@icloud.com>
2019-10-21 01:05:33 +0900

    템플릿 디렉토리 분류

--- src/template/tree/FenwickTree.java
+++ src/template/tree/FenwickTree.java
@@ -1,4 +1,4 @@
-package template;
+package template.tree;
 
 public class FenwickTree {
 
--- src/template/tree/SegmentTree.java
+++ src/template/tree/SegmentTree.java
@@ -1,4 +1,4 @@
-package template;
+package template.tree;
 
 public class SegmentTree {
     private int[] st;
