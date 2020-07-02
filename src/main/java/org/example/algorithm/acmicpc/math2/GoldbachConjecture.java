8246ee6f762928958c03658a974c2b08203b5855
Changehee Choi <changhee.dev@gmail.com>
2020-07-02 17:33:12 +0900

    메모리 사용량, 실행속도 개선

--- src/main/java/org/example/algorithm/acmicpc/math2/GoldbachConjecture.java
+++ src/main/java/org/example/algorithm/acmicpc/math2/GoldbachConjecture.java
@@ -1,7 +1,5 @@
 package org.example.algorithm.acmicpc.math2;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -16,30 +14,16 @@ public class GoldbachConjecture {
     private int count;
     private int[] numbers;
 
-    public int[][] solution() {
-        init2();
-        int[][] answer = new int[count][2];
+    public String[] solution() {
+        init();
+        String[] answer = new String[count];
 
         for (int i = 0; i < count; i++) {
-            int number = numbers[i];
-            ArrayList<Integer> primes = getPrimes(number);
-            Partition partition = selectPartition(number, primes);
-
-            answer[i][0] = partition.prime1;
-            answer[i][1] = partition.prime2;
+            String partition = selectPartition(numbers[i]);
+            answer[i] = partition;
         }
 
         return answer;
-    }
-
-    public void init2() {
-        count = 5;
-        numbers = new int[count];
-        numbers[0] = 8;
-        numbers[1] = 10;
-        numbers[2] = 16;
-        numbers[3] = 10000;
-        numbers[4] = 4;
     }
 
     public void init() {
@@ -53,33 +37,21 @@ public class GoldbachConjecture {
         sc.close();
     }
 
-    private ArrayList<Integer> getPrimes(int number) {
-        ArrayList<Integer> primes = new ArrayList<>();
-        for (int i = 1; i <= number; i++) {
-            if (isPrime(i)) {
-                primes.add(i);
+    private String selectPartition(int number) {
+        String partition = "";
+
+        int left = number / 2, right = number / 2;
+        while (left >= 2 && right <= number) {
+            //left는 1씩 줄이고 right는 1씩 늘리므로 항상 합은 number이기 때문에 조건으로 검사하지 않는다.
+            if (isPrime(left) && isPrime(right)) {
+                partition += left + " " + right;
+                break;
             }
-        }
-        return primes;
-    }
-
-    private Partition selectPartition(int number, ArrayList<Integer> primes) {
-
-        PriorityQueue<Partition> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < primes.size(); i++) {
-            if (2 * primes.get(i) == number) {
-                return new Partition(primes.get(i), primes.get(i));
-            }
-
-            for (int j = i; j < primes.size() - 1; j++) {
-                if (primes.get(i) + primes.get(j) == number) {
-                    pq.add(new Partition(primes.get(i), primes.get(j)));
-                }
-            }
+            left --;
+            right ++;
         }
 
-        return pq.peek();
+        return partition;
     }
 
     private boolean isPrime(int num) {
@@ -94,24 +66,5 @@ public class GoldbachConjecture {
             if (num % i == 0) return false;
         }
         return true;
-    }
-
-    class Partition implements Comparable<Partition> {
-        private int prime1;
-        private int prime2;
-
-        public Partition(int prime1, int prime2) {
-            this.prime1 = prime1;
-            this.prime2 = prime2;
-        }
-
-        public int getDiff() {
-            return this.prime2 - this.prime1;
-        }
-
-        @Override
-        public int compareTo(Partition o) {
-            return this.getDiff() - o.getDiff();
-        }
     }
 }
