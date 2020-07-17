990a25dad53f6820b172140f6b354dc7db762f93
Changehee Choi <changhee.dev@gmail.com>
2020-07-17 16:58:15 +0900

    문자열 - 문자열 집합 (트라이 사용)
    Map 을 이용한 트라이 구현체를 사용할 경우 500 * 10000 사이즈의 입력을 트라이로
    초기화하는 과정이 문제의 제한시간을 초과하여 배열을 사용하는 구현체로 수정하여 구현

--- src/main/java/org/example/algorithm/acmicpc/string/StringSet.java
+++ src/main/java/org/example/algorithm/acmicpc/string/StringSet.java
@@ -3,7 +3,9 @@ package org.example.algorithm.acmicpc.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 문자열 - 문자열 집합
@@ -13,7 +15,7 @@ import java.util.*;
  */
 public class StringSet {
 
-    private Set<String> sets; //List를 사용할 경우 실행시간 4816ms, Set 사용시 실행시간 464ms
+    private Trie trie;
     private List<String> words;
 
     public int solution() {
@@ -25,13 +27,13 @@ public class StringSet {
 
     private void init() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sets = new HashSet<>();
+        trie = new Trie();
         words = new ArrayList<>();
 
         try {
             int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             for (int i = 0; i < nums[0]; i++) {
-                sets.add(br.readLine());
+                trie.insert(br.readLine());
             }
 
             for (int i = 0; i < nums[1]; i++) {
@@ -51,8 +53,69 @@ public class StringSet {
     private int findWords() {
         int count = 0;
         for (String word : words) {
-            if (sets.contains(word)) count++;
+            if (trie.find(word)) count++;
         }
         return count;
+    }
+
+    class Trie {
+        private static final int ALPHABET_SIZE = 26;
+
+        class TrieNode {
+            private final TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+            private boolean isEndOfWord;
+
+            public TrieNode() {
+                this.isEndOfWord = false;
+                for (int i = 0; i < ALPHABET_SIZE; i++)
+                    children[i] = null;
+            }
+        }
+
+        private TrieNode root = new TrieNode();
+
+        /*단어 추가*/
+        public void insert(String word)
+        {
+            int level;
+            int length = word.length();
+            int index;
+
+            TrieNode current = root;
+
+            for (level = 0; level < length; level++)
+            {
+                index = word.charAt(level) - 'a';
+                if (current.children[index] == null)
+                    current.children[index] = new TrieNode();
+
+                current = current.children[index];
+            }
+
+            // mark last node as leaf
+            current.isEndOfWord = true;
+        }
+
+        /*단어 검색*/
+        public boolean find(String word) {
+            int level;
+            int length = word.length();
+            int index;
+
+            //루트 노드에서 시작
+            TrieNode current = root;
+
+            for (level = 0; level < length; level++)
+            {
+                index = word.charAt(level) - 'a';
+
+                if (current.children[index] == null)
+                    return false;
+
+                current = current.children[index];
+            }
+
+            return (current != null && current.isEndOfWord);
+        }
     }
 }
