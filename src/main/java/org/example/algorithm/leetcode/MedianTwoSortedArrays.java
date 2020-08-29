52d69e2e9e9fed8b48e60aedd8d94293ae04d955
Changehee Choi <changhee.dev@gmail.com>
2020-08-29 19:28:33 +0900

    시간복잡도, 공간 복잡도 개선
    시간복잡도 : O(n+m) -> O(log(n+m))
    공간복잡도 : O(n+m) -> O(1)

--- src/main/java/org/example/algorithm/leetcode/MedianTwoSortedArrays.java
+++ src/main/java/org/example/algorithm/leetcode/MedianTwoSortedArrays.java
@@ -8,45 +8,56 @@ package org.example.algorithm.leetcode;
  * @since 29/08/2020
  */
 public class MedianTwoSortedArrays {
+    /**
+     * 두 배열을 아래 두 조건을 만족하도록 partition 한다면 median = (max_left + min_right) / 2
+     * 조건1. len(left_part_set1) + len(left_part_set2) == len(right_part_set1) + len(right_part_set2)
+     * 조건2. max(left_part_set1, left_part_set2) <= min(right_part_set1, right_part_set2)
+     */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] mergedArr = new int[nums1.length + nums2.length];
-        int nums1Idx = 0, nums2Idx = 0, mergedIdx = 0;
-        double answer = 0;
+        //nums1.length <= nums2.length 인 상태가 되도록 재귀 호출
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        while (nums1Idx < nums1.length && nums2Idx < nums2.length) {
-            if (nums1[nums1Idx] < nums2[nums2Idx]) {
-                mergedArr[mergedIdx++] = nums1[nums1Idx++];
-            } else if (nums1[nums1Idx] > nums2[nums2Idx]) {
-                mergedArr[mergedIdx++] = nums2[nums2Idx++];
+        int nums1Len = nums1.length, nums2Len = nums2.length;
+        int halfLen = (nums1Len + nums2Len + 1) / 2;
+
+        int left = 0, right = nums1.length;
+
+        while (left <= right) {
+            //0 ~ (partitionNums1 - 1) : left part of nums1
+            //partitionNums1 ~ (nums1Len - 1): right part of nums1
+            int partitionNums1 = (left + right) / 2;
+
+            //nums1, nums2 배열을 merge 했을 때의 중간 지점에서 num1의 left part element 개수를 빼면
+            //nums2의 left part element 개수가 된다.
+            int partitionNums2 = halfLen - partitionNums1;
+
+            //if partition == 0 (left part is empty set), then maxLeft = -INF
+            int maxLeftNums1 = (partitionNums1 == 0) ? Integer.MIN_VALUE : nums1[partitionNums1 - 1];
+            //if partition == length (right part is empty set), then minRight = INF
+            int minRightNums1 = (partitionNums1 == nums1Len) ? Integer.MAX_VALUE : nums1[partitionNums1];
+
+            int maxLeftNums2 = (partitionNums2 == 0) ? Integer.MIN_VALUE : nums2[partitionNums2 - 1];
+            int minRightNums2 = (partitionNums2 == nums2Len) ? Integer.MAX_VALUE : nums2[partitionNums2];
+
+            //두 배열의 left part의 합집합의 원소가 right part의 합집합의 원소보다 같거나 작으려면
+            //maxLeftSet1 <= minRightSet2, maxLeftSet2 <= minRightSet1 조건을 만족하여야 한다
+            //조건을 만족하는 partition 위치를 찾는다
+            if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1) {
+                if ((nums1Len + nums2Len) % 2 == 0) {
+                    return (double) (Math.max(maxLeftNums1, maxLeftNums2) + Math.min(minRightNums1, minRightNums2)) / 2;
+                } else {
+                    return Math.max(maxLeftNums1, maxLeftNums2);
+                }
+            } else if (maxLeftNums1 > minRightNums2) {
+                right = partitionNums1 - 1;
             } else {
-                mergedArr[mergedIdx++] = nums1[nums1Idx++];
-                mergedArr[mergedIdx++] = nums2[nums2Idx++];
+                left = partitionNums1 + 1;
             }
         }
 
-        if (nums1Idx >= nums1.length) {
-            while (nums2Idx < nums2.length) {
-                mergedArr[mergedIdx++] = nums2[nums2Idx++];
-            }
-        } else {
-            while (nums1Idx < nums1.length) {
-                mergedArr[mergedIdx++] = nums1[nums1Idx++];
-            }
-        }
-
-        if (mergedArr.length == 0) {
-            return 0;
-        }
-
-        int mid = mergedArr.length / 2;
-
-        if (mergedArr.length % 2 == 0) {
-            answer = (double) (mergedArr[mid] + mergedArr[mid - 1]) / 2.0;
-        } else {
-            answer = mergedArr[mid];
-        }
-
-        return answer;
+        return -1;
     }
 
     public void runTest(int[] nums1, int[] nums2) {
