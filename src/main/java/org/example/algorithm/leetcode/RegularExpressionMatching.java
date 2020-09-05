5a3ac40f72c66783a013d0ed7de8d933f477a0ea
Changehee Choi <changhee.dev@gmail.com>
2020-09-05 22:43:01 +0900

    leetcode - Regular Expression Matching (DP 적용 버전)

--- src/main/java/org/example/algorithm/leetcode/RegularExpressionMatching.java
+++ src/main/java/org/example/algorithm/leetcode/RegularExpressionMatching.java
@@ -8,17 +8,44 @@ package org.example.algorithm.leetcode;
  * @since 05/09/2020
  */
 public class RegularExpressionMatching {
-    public boolean isMatch(String s, String p) {
-        if (p.isEmpty()) return s.isEmpty();
-        boolean first_match = (!s.isEmpty() &&
-                (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+    Boolean[][] memo;
 
-        if (p.length() >= 2 && p.charAt(1) == '*') {
-            // * : 0 또는 그 이상 매칭됨
-            //return (0번 매칭 || 1번이상 매칭)
-            return (isMatch(s, p.substring(2)) || (first_match && isMatch(s.substring(1), p)));
-        } else {
-            return first_match && isMatch(s.substring(1), p.substring(1));
+    public boolean isMatch(String text, String pattern) {
+        memo = new Boolean[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    /**
+     * @param i       문자열 인덱스
+     * @param j       패턴 인덱스
+     * @param text    문자열
+     * @param pattern 패턴
+     * @return 매칭 여부
+     */
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (memo[i][j] != null) {
+            return memo[i][j] == true;
         }
+
+        boolean ans;
+
+        //모든 패턴을 확인
+        if (j == pattern.length()) {
+            //모든 문자가 패턴에 매칭 되었는지 여부
+            ans = i == text.length();
+            memo[i][j] = ans;
+            return ans;
+        }
+
+        boolean first_match = (i < text.length() && (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
+
+        if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+            ans = dp(i, j + 2, text, pattern) || (first_match && dp(i + 1, j, text, pattern));
+        } else {
+            ans = first_match && dp(i + 1, j + 1, text, pattern);
+        }
+
+        memo[i][j] = ans;
+        return ans;
     }
 }
