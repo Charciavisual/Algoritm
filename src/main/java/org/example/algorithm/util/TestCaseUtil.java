174c4be4fc7b3bc5c24fe4c9254fa1b5f90fa926
Changehee Choi <changhee.dev@gmail.com>
2020-09-25 00:49:41 +0900

    테스트 케이스 생성 유틸 클래스 구현

---
+++ src/main/java/org/example/algorithm/util/TestCaseUtil.java
@@ -0,0 +1,75 @@
+package org.example.algorithm.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Changhee Choi
+ * @since 27/08/2020
+ */
+public class TestCaseUtil {
+
+    public static String generateString(int len) {
+        return generateRandomString(len, Arrays.asList(0, 1, 2));
+    }
+
+    public static String generateString(int len, boolean lower, boolean upper, boolean digit) {
+        if (!lower && !upper && !digit) {
+            throw new IllegalArgumentException("At least one option must be true");
+        }
+
+        List<Integer> options = new ArrayList<>();
+
+        if (lower) {
+            options.add(0);
+        }
+        if (upper) {
+            options.add(1);
+        }
+        if (digit) {
+            options.add(2);
+        }
+
+        return generateRandomString(len, options);
+    }
+
+    private static String generateRandomString(int len, List<Integer> options) {
+        StringBuilder builder = new StringBuilder();
+        Random rnd = new Random();
+        int bound = options.size();
+        for (int i = 0; i < len; i++) {
+            int rIndex = rnd.nextInt(bound);
+            switch (options.get(rIndex)) {
+                case 0:
+                    // a-z
+                    builder.append((char) ((rnd.nextInt(26)) + 97));
+                    break;
+                case 1:
+                    // A-Z
+                    builder.append((char) ((rnd.nextInt(26)) + 65));
+                    break;
+                case 2:
+                    // 0-9
+                    builder.append((rnd.nextInt(10)));
+                    break;
+            }
+        }
+        return builder.toString();
+    }
+
+    public static int generateRandomNumber(int min, int max) {
+        Random rnd = new Random();
+        int number = rnd.nextInt(max - min + 1) + min;
+        return number;
+    }
+
+    public static int[] generateRandomNumbers(int size, int min, int max) {
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = generateRandomNumber(min, max);
+        }
+        return numbers;
+    }
+}
---
+++ src/test/java/org/example/algorithm/util/TestCaseUtilTests.java
@@ -0,0 +1,71 @@
+package org.example.algorithm.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Changhee Choi
+ * @since 27/08/2020
+ */
+public class TestCaseUtilTests {
+
+    @Test
+    void option을_사용하지_않는_경우_소문자_대문자_숫자가_조합된_문자열이_생성된다() {
+        int loop = 100000;
+
+        for (int i = 0; i < loop; i++) {
+            String randomString = TestCaseUtil.generateString(100);
+            assertThat(randomString.length()).isEqualTo(100);
+            assertThat(Pattern.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).+$", randomString)).isTrue();
+        }
+    }
+
+    @Test
+    void lower_options이_false이면_소문자가_포함되지_않는_문자열이_생성된다() {
+        int loop = 100000;
+
+        for (int i = 0; i < loop; i++) {
+            String randomString = TestCaseUtil.generateString(100, false, true, true);
+            assertThat(randomString.length()).isEqualTo(100);
+            assertThat(Pattern.matches("^[^a-z]+$", randomString)).isTrue();
+        }
+    }
+
+    @Test
+    void upper_options이_false이면_대문자가_포함되지_않는_문자열이_생성된다() {
+        int loop = 100000;
+
+        for (int i = 0; i < loop; i++) {
+            String randomString = TestCaseUtil.generateString(100, true, false, true);
+            assertThat(randomString.length()).isEqualTo(100);
+            assertThat(Pattern.matches("^[^A-Z]+$", randomString)).isTrue();
+        }
+    }
+
+    @Test
+    void digit_options이_false이면_숫자가_포함되지_않는_문자열이_생성된다() {
+        int loop = 100000;
+
+        for (int i = 0; i < loop; i++) {
+            String randomString = TestCaseUtil.generateString(100, true, true, false);
+            assertThat(randomString.length()).isEqualTo(100);
+            assertThat(Pattern.matches("^[^0-9]+$", randomString)).isTrue();
+        }
+    }
+
+    @Test
+    void 생성된_숫자는_모두_min_이상의_값이다() {
+        int[] numbers = TestCaseUtil.generateRandomNumbers(10000, 5, 100);
+        assertThat(Arrays.stream(numbers).filter(number -> number < 5).count()).isEqualTo(0);
+    }
+
+    @Test
+    void 생성된_숫자는_모두_max_이하의_값이다() {
+        int[] numbers = TestCaseUtil.generateRandomNumbers(10000, 5, 100);
+        assertThat(Arrays.stream(numbers).filter(number -> number > 100).count()).isEqualTo(0);
+    }
+}
