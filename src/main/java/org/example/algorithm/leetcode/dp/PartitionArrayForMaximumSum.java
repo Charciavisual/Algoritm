503a6491d283d6ff13eb91333cb141781f22de92
Changehee Choi <changhee.dev@gmail.com>
2020-12-24 00:13:01 +0900

    refactor: 문제 링크 추가 및 reformat

--- src/main/java/org/example/algorithm/leetcode/dp/PartitionArrayForMaximumSum.java
+++ src/main/java/org/example/algorithm/leetcode/dp/PartitionArrayForMaximumSum.java
@@ -1,23 +1,24 @@
 package org.example.algorithm.leetcode.dp;
 
 /**
+ * @see <a>https://leetcode.com/problems/partition-array-for-maximum-sum/</a>
  * @author Changhee Choi
  * @since 23/12/2020
  */
 public class PartitionArrayForMaximumSum {
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        final int n = arr.length;
-        final int[] dp = new int[n + 1];
+  public int maxSumAfterPartitioning(int[] arr, int k) {
+    final int n = arr.length;
+    final int[] dp = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            int maxValue = 0;
-            for (int j = 0; i - j >= 0 && j < k; j++) {
-                maxValue = Math.max(maxValue, arr[i - j]);
-                int partitionSum = maxValue * (j + 1);
-                dp[i + 1] = Math.max(dp[i + 1], partitionSum + dp[i - j]);
-            }
-        }
-
-        return dp[n];
+    for (int i = 0; i < n; i++) {
+      int maxValue = 0;
+      for (int j = 0; i - j >= 0 && j < k; j++) {
+        maxValue = Math.max(maxValue, arr[i - j]);
+        int partitionSum = maxValue * (j + 1);
+        dp[i + 1] = Math.max(dp[i + 1], partitionSum + dp[i - j]);
+      }
     }
+
+    return dp[n];
+  }
 }
